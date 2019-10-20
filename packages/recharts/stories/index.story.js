8524c51272e9cc5affb1eb914af2a7d72b4c467f
jafarRezaei <bomber.man87@yahoo.com>
2019-10-20 07:32:17 +0330

    add line charts to re-charts

--- packages/recharts/stories/index.story.js
+++ packages/recharts/stories/index.story.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 
 // import notes with promise
 import NotePromise from './../notes/Basic';
-import { boolean, text, withKnobs } from "@storybook/addon-knobs";
+import { boolean, text, withKnobs } from '@storybook/addon-knobs';
 import { withSmartKnobs } from 'storybook-addon-smart-knobs';
 
 // wait for notes promise then show story
@@ -18,17 +18,200 @@ NotePromise.then(notes =>
     //   </button>
     // ))
 
+    // LINE
     .add(
-    'Basic',
-    () => {
-      const Basic = lazy(() => import('./../src/AreaChart'));
-      return (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Basic />
-        </Suspense>
-      );
-    },
-    { notes }
-  )
-);
+      'LineChart/Simple',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/Simple'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/Tiny',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/Tiny'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/Dashed',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/Dashed'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/Vertical',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/Vertical'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/Biaxial',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/Biaxial'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/VerticalWithDomain',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/VerticalWithDomain'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/LineChartConnectedNull',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/LineChartConnectedNull'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/xAxisPadding',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/xAxisPadding'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/ReferenceLine',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/ReferenceLine'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/CustomDotLine',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/CustomDotLine'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/CustomLabel',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/CustomLabel'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/SyncedCharts',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/SyncedCharts'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/HighlightAndZoom',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/HighlightAndZoom'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/MultiSeries',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/MultiSeries'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+    .add(
+      'LineChart/AxisInterval',
+      () => {
+        const Basic = lazy(() => import('./../src/LineChart/AxisInterval'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
 
+
+
+    .add(
+      'Basic',
+      () => {
+        const Basic = lazy(() => import('./../src/AreaChart'));
+        return (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Basic />
+          </Suspense>
+        );
+      },
+      { notes }
+    )
+);
